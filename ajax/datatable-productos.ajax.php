5a0c130183291bf7f10224791530ecf3008e7353
Juan Camilo Garcia <jcgm1047@gmail.com>
2022-10-03 18:17:53 -0500

    Hasta aqui todo funciona

--- ajax/datatable-productos.ajax.php
+++ ajax/datatable-productos.ajax.php
@@ -16,31 +16,44 @@ class TablaProductos
 
     $productos = ControladorProductos::ctrMostrarProductos($item, $valor);
 
-
-
-    $img = "<img src='view/img/productos/101/105.png' width='40px'>";
-    $btn = "<div class='btn-group'><button class='btn btn-warning'> <i class='fa fa-pencil'></i> </button><button class='btn btn-danger'> <i class='fa fa-times'></i> </button></div>";
-
     $datosJson = '{
     "data": [';
     for ($i = 0; $i < count($productos); $i++) {
 
+
+      /* Trae La imagen */
       $img = "<img src='" . $productos[$i]["imagen"] . "' width='40px'>";
 
+      /* Trae la Categoria */
       $item = "id";
       $valor = $productos[$i]["id_categoria"];
       $categorias = ControladorCategorias::ctrMostrarCategorias($item, $valor);
 
+      /* Mostrar estado de stock */
+
+      if ($productos[$i]["stock"] <= 10) {
+        $stock = "<button class='btn btn-danger'>" . $productos[$i]["stock"] . "</button>";
+      } else if ($productos[$i]["stock"] > 11 && $productos[$i]["stock"] < 20) {
+        $stock = "<button class='btn btn-warning'>" . $productos[$i]["stock"] . "</button>";
+      } else {
+        $stock = "<button class='btn btn-success'>" . $productos[$i]["stock"] . "</button>";
+      }
+
+
+
+      /* Trae los botones */
+      $btn = "<div class='btn-group'><button class='btn btn-warning btnEditarProducto' idProducto= '" . $productos[$i]["id"] . "' data-toggle='modal' data-target=#modalEditarProducto> <i class='fa fa-pencil'></i></button><button class='btn btn-danger btnEliminarProducto' idProducto= '" . $productos[$i]["id"] . "' codigo = '" . $productos[$i]["codigo"] . "' imagen = '" . $productos[$i]["imagen"] . "'><i class='fa fa-times'></i> </button></div>";
 
 
 
       $datosJson .= '[
       "' . $i + 1 . '",
       "' . $img . '",
+
       "' . $productos[$i]["codigo"] . '",
       "' . $productos[$i]["descripcion"] . '",
       "' . $categorias["categoria"] . '",
-      "' . $productos[$i]["stock"] . '",
+      "' .  $stock . '",
       "' . $productos[$i]["precio_compra"] . '",
       "' . $productos[$i]["precio_venta"] . '",
       "' . $productos[$i]["fecha"] . '",                
